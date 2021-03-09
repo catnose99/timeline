34a5ff2ca20cac7ee06382d656f6e95df9988774
catnose99 <nodding.com@gmail.com>
2021-03-09 04:34:53 +0000

    fix: enhance profile

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -1,4 +1,5 @@
 import styles from '../styles/components/HomeHero.module.scss';
+import { TwitterIcon } from './TwitterIcon';
 
 export const HomeHero = () => {
   return (
@@ -6,8 +7,14 @@ export const HomeHero = () => {
       <img src="/paw.svg" alt="Hello" width={51} height={58} />
       <h1 className={styles.title}>Hi, I'm catnose</h1>
       <p className={styles.description}>
-        Designer, developer, maker, dog &amp; cat lover. Currently working on
-        zenn.dev with Classmethod. Follow @catnose99 for daily updates.
+        Designer, developer, maker, dog &amp; cat lover. Currently working on{' '}
+        <a href="https://zenn.dev">zenn.dev</a> with{' '}
+        <a href="https://classmethod.jp">Classmethod</a>. Follow{' '}
+        <a href="https://twitter.com/catnose99">
+          @catnose99
+          <TwitterIcon width={18} height={18} />
+        </a>{' '}
+        for daily updates.
       </p>
     </div>
   );
