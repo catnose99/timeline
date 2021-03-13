a1e186a42a03795ab69bce813264664418c9ff07
catnose99 <nodding.com@gmail.com>
2021-03-13 13:01:11 +0000

    fix: styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           type="image/png"
           href={`${siteConfig.siteRoot}/icon.png`}
         />
+        <link
+          rel="apple-touch-icon-precomposed"
+          type="image/png"
+          href={`${siteConfig.siteRoot}/icon.png`}
+        />
         <meta property="og:image" content={`${siteConfig.siteRoot}/icon.png`} />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary" />
